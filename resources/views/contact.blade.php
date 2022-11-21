4410959c6c3c6f8c337cb7aa27a1619dbbc520bd
AHMED NABIB <nabibpallab@gmail.com>
2022-11-21 16:28:19 +0600

    custome authintication complete

--- resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>hello contact page</h1>
-
-<form action="{{ route('student.store')}}">
-    @csrf
-    <input type="text" name="name" id="">
-    <input type="email" name="email" id="">
-    <button type="submit">submit</button>
-</form>
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\SingleController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\SampleController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('registration');
 });
 
 
@@ -40,6 +41,23 @@ Route::get('/test',SingleController::class);
 Route::resource("/student", StudentController::class);
 
 Route::resource('image', ImageController::class);
+
+
+// Route::controller(SampleController::class)->group(function(){
+
+//     Route::get('login', 'index')->name('login');
+
+//     Route::get('registration', 'registration')->name('registration');
+
+//     Route::get('logout', 'logout')->name('logout');
+
+//     Route::post('validate_registration', 'validate_registration')->name('sample.validate_registration');
+
+//     Route::post('validate_login', 'validate_login')->name('sample.validate_login');
+
+//     Route::get('dashboard', 'dashboard')->name('dashboard');
+
+// });
 
 
 
